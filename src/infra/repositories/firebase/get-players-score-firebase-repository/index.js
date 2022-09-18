5b8dfa1e4bf406545b0e4015c5e5e8778662ed36
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-18 17:43:17 -0300

    feat: garante que getPlayersScore irá retornar scores do jogadores no formato correto

--- src/infra/repositories/firebase/get-players-score-firebase-repository/index.js
+++ src/infra/repositories/firebase/get-players-score-firebase-repository/index.js
@@ -1,27 +1,55 @@
 export class GetPlayersScoreFirebaseRepository {
-  #firebaseDatabase
-  constructor({
-    firebaseDatabase
-  }) {
-    /**
-     * @type {import('firebase/compat/database/dist/database/index').DatabaseReference}
+   /**
+     * @type {import('firebase/compat/index').default.database.Database}
      */
+  #firebaseDatabase
+
+  constructor({ firebaseDatabase }) { 
     this.#firebaseDatabase = firebaseDatabase
   }
-  async getPlayersScore({ playerName = '' } = {}) {
-    const playersRef = this.#firebaseDatabase.ref(`ranking/${playerName}`)
-    return new Promise((resolve, reject) => {
-      playersRef.on('value', player => {
-        const playerData = player.val()
-        const parsedPlayerData = Object.entries(playerData).map(
-          ([key, value]) => ({
-            playerName: key,
-            score: value,
-          })
-        )
 
-        return resolve(parsedPlayerData)
+   onPlayerScoreValue(context = {}) {
+    const { player, playerName} = context
+    const playerData = player.val()
+
+    if (!playerData) {
+      return []
+    }
+
+    if (playerName) {
+      return [
+        {
+          playerName,
+          score: playerData,
+        },
+      ]
+    }
+    const parsedPlayerData = Object.entries(playerData).map(
+      ([key, value]) => ({
+        playerName: key,
+        score: value,
       })
+    )
+
+    return parsedPlayerData
+  }
+
+
+  async getPlayersScore({ playerName = '' } = {}) {
+    return new Promise((resolve, reject) => {
+      try {
+        const playersRef = this.#firebaseDatabase.ref(`ranking/${playerName}`)
+        playersRef.on('value', player => {
+          const parsedPlayerData = this.onPlayerScoreValue({
+            player,
+            playerName,
+          })
+
+          resolve(parsedPlayerData)
+        })
+      } catch (error) {
+        reject(error)
+      }
     })
   }
 }
