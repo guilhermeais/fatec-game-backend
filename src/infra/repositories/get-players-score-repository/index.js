ab7316c5af6ca902ea0e1298858e9863c6cebc3b
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-18 02:03:04 -0300

    feat: cria GetPlayersScoreRepository

---
+++ src/infra/repositories/get-players-score-repository/index.js
@@ -0,0 +1,27 @@
+export class GetPlayersScoreRepository {
+  #firebaseDatabase
+  constructor({
+    firebaseDatabase
+  }) {
+    /**
+     * @type {import('firebase/compat/database/dist/database/index').DatabaseReference}
+     */
+    this.#firebaseDatabase = firebaseDatabase
+  }
+  async getPlayersScore({ playerName = '' } = {}) {
+    const playersRef = this.#firebaseDatabase.ref(`ranking/${playerName}`)
+    return new Promise((resolve, reject) => {
+      playersRef.on('value', player => {
+        const playerData = player.val()
+        const parsedPlayerData = Object.entries(playerData).map(
+          ([key, value]) => ({
+            playerName: key,
+            score: value,
+          })
+        )
+
+        return resolve(parsedPlayerData)
+      })
+    })
+  }
+}
