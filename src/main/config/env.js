e510c088709dba9037a3112091ea5e0f33689521
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-18 02:01:10 -0300

    chore: adiciona .env

---
+++ src/main/config/env.js
@@ -0,0 +1,11 @@
+export const env = {
+  port: process.env.PORT || 3000,
+  
+  firebaseApiKey: process.env.FIREBASE_API_KEY,
+  firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  firebaseDatabaseURL: process.env.FIREBASE_DATABASE_URL,
+  firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+  firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  firebaseAppId: process.env.FIREBASE_APP_ID,
+}
