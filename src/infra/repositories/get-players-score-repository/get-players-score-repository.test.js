713543b7f07f8659a6fedc3e0f1d93de864b930c
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-18 02:03:10 -0300

    test: cria GetPlayersScoreRepository

---
+++ src/infra/repositories/get-players-score-repository/get-players-score-repository.test.js
@@ -0,0 +1,47 @@
+import { afterAll, beforeEach, describe, expect, test } from 'vitest'
+import { GetPlayersScoreRepository } from '.'
+import firebaseTestHelpers from '../../../../tests/helpers/firebase-test-helpers'
+
+async function makePlayerScore() {
+  const result = await firebaseTestHelpers.insertDataWithRef('ranking', {
+    guilherme: 100,
+  })
+
+  return Object.entries(result).map(([key, value]) => ({
+    playerName: key,
+    score: value,
+  }))
+}
+
+let database
+const projectId = 'test-project'
+describe('GetPlayerScoreRepository', () => {
+  function makeSut(firebaseDatabase) {
+    const sut = new GetPlayersScoreRepository({
+      firebaseDatabase,
+    })
+
+    return {
+      sut,
+    }
+  }
+  beforeEach(async () => {
+    await firebaseTestHelpers.connect({ projectId })
+    database = firebaseTestHelpers.database
+    firebaseTestHelpers.testEnvironment.clearDatabase({projectId})
+  })
+
+  afterAll(async () => {
+    await firebaseTestHelpers.reset()
+  })
+  describe('getPlayersScore', () => {
+    test('should return all the players', async () => {
+      const playerResult = await makePlayerScore()
+
+      const { sut } = makeSut(database)
+
+      const players = await sut.getPlayersScore()
+      expect(players).toEqual(playerResult)
+    })
+  })
+})
