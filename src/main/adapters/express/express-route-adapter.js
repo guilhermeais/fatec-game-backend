2d8b7141348babe96ae858e20a68bb96afb3bd56
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-18 22:33:30 -0300

    refactor: arruma nomenclaturas de variáveis

--- src/main/adapters/express/express-route-adapter.js
+++ src/main/adapters/express/express-route-adapter.js
@@ -1,11 +1,11 @@
 export function adaptRoute (controller) {
   return async function (req, res) {
-    const httpRequest= {
+    const request= {
       ...(req.body || {}),
       ...(req.params || {}),
     }
     try {
-      const httpResponse = await controller.handle(httpRequest)
+      const httpResponse = await controller.handle(request)
       if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
         return res.status(httpResponse.statusCode).json(httpResponse.body)
       }
