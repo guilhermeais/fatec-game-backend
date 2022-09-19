ee6eede9b44e0e1940d9e5edba46de985a08d8d4
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-18 22:34:32 -0300

    feat: adiciona parametro playerName na PlayerRoutes

--- src/main/routes/players-routes.js
+++ src/main/routes/players-routes.js
@@ -2,5 +2,5 @@ import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeGetPlayersScoreController } from '../factories/controllers/get-players-score-controller-factory'
 
 export function setupPlayersRoutes (router) {
-  router.get('/players/score', adaptRoute(makeGetPlayersScoreController()))
+  router.get('/players/:playerName?/score', adaptRoute(makeGetPlayersScoreController()))
 }
