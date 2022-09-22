d536732bde846d62ae8a31543002b34d54941760
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-22 20:28:11 -0300

    feat: adiciona middleware cors

---
+++ src/main/middlewares/cors.js
@@ -0,0 +1,7 @@
+export const cors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+
+  next()
+}
