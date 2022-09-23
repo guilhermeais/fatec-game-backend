282fbd7c100dd62cbed9b73140679c892a865863
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-23 08:05:16 -0300

    feat: utiliza content-type middlware

--- src/main/config/middlewares.js
+++ src/main/config/middlewares.js
@@ -1,6 +1,7 @@
-import { bodyParser, cors } from '../middlewares'
+import { bodyParser, contentType, cors } from '../middlewares'
 
 export function setupMiddlewares(app) {
-  app.use(cors)
   app.use(bodyParser)
+  app.use(contentType)
+  app.use(cors)
 }
